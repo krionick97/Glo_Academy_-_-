d4f065eb18d727674fe5b8a3bc3af714ade3a30e
krionick97 <mistermers8@gmail.com>
2018-12-08 01:57:03 +0300

    Corrections

--- mailer/smart.php
+++ mailer/smart.php
@@ -12,13 +12,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'mail@mail.ru';                 // Наш логин
-$mail->Password = '032ujfw9';                           // Наш пароль от ящика
+$mail->Username = 'stechkin-stalker@mail.ru';                 // Наш логин
+$mail->Password = 'xb86Mp13rTz';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('mail@mail.ru', 'John Doe');   // От кого письмо 
-$mail->addAddress('mail@yandex.ru');     // Add a recipient
+$mail->addAddress('mistermers8@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
